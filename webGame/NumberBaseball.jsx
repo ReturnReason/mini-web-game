4d283d459def02e7081333c4036d815a7104bf79
reason <reasonz@daum.net>
2022-07-16 22:35:04 +0900

    리액트로 숫자야구게임 만들기(클래스형 컴포넌트)

---
+++ webGame/NumberBaseball.jsx
@@ -0,0 +1,111 @@
+// import React, { Component } from 'react';
+// import style from './styles/style.css';
+
+function newGame() {
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+
+  for (let i = 0; i < 4; i++) {
+    array.push(candidate.splice(Math.floor(Math.random() * 9 - i), 1)[0]);
+  }
+  return array;
+}
+
+class NumberBaseball extends Component {
+  state = {
+    userInputValue: '',
+    quiz: newGame(),
+    result: [],
+  };
+
+  timer = () => {
+    setTimeout(() => {
+      this.setState({
+        userInputValue: '',
+        quiz: newGame(),
+        result: [],
+      });
+    }, 3000);
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const currentQuiz = [...this.state.quiz];
+    let currentResult = '';
+
+    // 현재 문제 정답을 string으로 타입 변환 뒤 + 연산하여 문자 이어붙이기
+    currentQuiz.forEach((v) => {
+      currentResult += v.toString();
+    });
+
+    if (this.state.result.length >= 10) {
+      this.timer();
+      this.setState({
+        result: [...this.state.result, { comment: `10회 도전했으나 실패하여 3초 후 새게임을 시작합니다.` }],
+        quiz: newGame(),
+        userInputValue: '',
+      });
+      alert('10회 도전했으나 실패하여 3초 뒤 새게임을 시작합니다.');
+    } else {
+      if (currentResult === this.state.userInputValue) {
+        this.setState({
+          result: [],
+          quiz: newGame(),
+          userInputValue: '',
+        });
+
+        alert('홈런! 새 게임을 시작합니다.');
+      } else {
+        let strike = 0;
+        let ball = 0;
+
+        for (let i = 0; i < 4; i++) {
+          if (currentResult[i] === this.state.userInputValue[i]) {
+            strike += 1;
+          } else if (this.state.userInputValue.includes(currentResult[i])) {
+            ball += 1;
+          }
+        }
+
+        this.setState({
+          result: [...this.state.result, { answer: this.state.userInputValue, comment: `${strike} 스트라이크, ${ball} 볼 입니다!` }],
+          userInputValue: '',
+        });
+      }
+    }
+  };
+
+  onChange = (e) => {
+    this.setState({
+      userInputValue: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="bg">
+        <h2 className="title">
+          <span className="baseball-icon">⚾</span> 숫자 야구 게임 <span className="baseball-icon">⚾</span>
+        </h2>
+        <p className="sub-text">1 ~ 9 중 4가지 숫자를 입력하세요.</p>
+        <form className="game-container" onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} value={this.state.userInputValue} className="input-box" type="number" />
+          <button className="btn">확인</button>
+        </form>
+        <ul className="result-container">
+          {this.state.result.map((v, i) => {
+            return (
+              <li key={`${i}번째 도전`}>
+                {' '}
+                {v.answer} ▶ {v.comment}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default NumberBaseball2;
